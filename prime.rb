c130b3780fff6d48ed9711ce055b82f6daa7ddb1
Julian <julianrosenthal@yahoo.com>
2020-11-10 18:15:32 -0700

    commit tree 3e87aee05ebb2e40896f5106a8e2b894b3b607eb

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+    return true if num == 2
+    return false if num < 2 || num.even? 
+    i = 3
+    while i <= Math.sqrt(num) ##Learned that while working with node, much faster
+        return false if num % i == 0
+        i += 2
+    end
+    return true
+end
